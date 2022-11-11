691e75110ccb74f71404fe19239d4088d5dcfb7b
雷泽龙 <lei.zelong@xyb2b.com>
2022-11-11 14:25:17 +0800

    feat: 加载本地html

--- src/extension.ts
+++ src/extension.ts
@@ -14,8 +14,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('swagger-generate-ts.definitions', generateDefinitions);
+	const extensionPath  = context.extensionPath;
 
+	let disposable = vscode.commands.registerCommand('swagger-generate-ts.definitions', generateDefinitions(extensionPath));
 	context.subscriptions.push(disposable);
 }
 
